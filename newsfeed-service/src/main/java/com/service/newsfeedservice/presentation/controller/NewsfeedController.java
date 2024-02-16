b5a67a9db9ed7acdfe27370da2d6a5a96ee5c424
deepsj1012 <deepsj1012@naver.com>
2024-02-16 14:42:15 +0900

    모듈 분리 후, API GATEWAY 설정 완료

--- newsfeed-service/src/main/java/com/service/newsfeedservice/presentation/controller/NewsfeedController.java
+++ newsfeed-service/src/main/java/com/service/newsfeedservice/presentation/controller/NewsfeedController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/newsfeed")
+@RequestMapping("/newsfeeds")
 public class NewsfeedController {
     private final NewsfeedService newsfeedService;
 
