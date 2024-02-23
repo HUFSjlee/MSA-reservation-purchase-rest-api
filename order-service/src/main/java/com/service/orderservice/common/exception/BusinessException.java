656bb643fde8547215c018ed206df42c6a7140ff
deepsj1012 <deepsj1012@naver.com>
2024-02-23 18:09:21 +0900

    BusinessException 추가

--- order-service/src/main/java/com/service/orderservice/common/exception/BusinessException.java
+++ order-service/src/main/java/com/service/orderservice/common/exception/BusinessException.java
@@ -1,6 +1,7 @@
 package com.service.orderservice.common.exception;
 
-import com.service.productservice.common.response.ResponseCode;
+
+import com.service.orderservice.common.response.ResponseCode;
 
 public class BusinessException extends RuntimeException {
     protected ResponseCode responseCode;
