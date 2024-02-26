654bedfc118488f8f7fd510d3267f4dea74586b1
deepsj1012 <deepsj1012@naver.com>
2024-02-26 12:39:15 +0900

    게시글 관련 예외 처리

---
+++ product-service/src/main/java/com/service/productservice/common/exception/NotFoundPostException.java
@@ -0,0 +1,17 @@
+package com.service.productservice.common.exception;
+
+import com.service.productservice.common.response.ResponseCode;
+
+public class NotFoundPostException extends BusinessException {
+    public NotFoundPostException() {
+        super(ResponseCode.NOT_FOUND_RESOURCE, ResponseCode.NOT_FOUND_RESOURCE.getDefaultMessage());
+    }
+
+    public NotFoundPostException(String customMessage) {
+        super(ResponseCode.NOT_FOUND_RESOURCE, customMessage);
+    }
+
+    public NotFoundPostException(ResponseCode responseCode, String customMessage) {
+        super(responseCode, customMessage);
+    }
+}
