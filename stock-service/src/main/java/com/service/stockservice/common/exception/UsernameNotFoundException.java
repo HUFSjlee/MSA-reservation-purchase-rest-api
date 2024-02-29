79bb0e949543c5362f372dfb01679a0eaed20f5c
deepsj1012 <deepsj1012@naver.com>
2024-02-29 15:08:14 +0900

    Custom Exception Class 추가

---
+++ stock-service/src/main/java/com/service/stockservice/common/exception/UsernameNotFoundException.java
@@ -0,0 +1,18 @@
+package com.service.stockservice.common.exception;
+
+
+import com.service.stockservice.common.response.ResponseCode;
+
+public class UsernameNotFoundException extends BusinessException {
+    public UsernameNotFoundException() {
+        super(ResponseCode.NOT_FOUND_RESOURCE, ResponseCode.NOT_FOUND_RESOURCE.getDefaultMessage());
+    }
+
+    public UsernameNotFoundException(String customMessage) {
+        super(ResponseCode.NOT_FOUND_RESOURCE, customMessage);
+    }
+
+    public UsernameNotFoundException(ResponseCode responseCode, String customMessage) {
+        super(responseCode, customMessage);
+    }
+}
