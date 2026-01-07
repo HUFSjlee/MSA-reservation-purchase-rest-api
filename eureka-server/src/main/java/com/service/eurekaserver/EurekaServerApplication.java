d36059d79896527e45c947be42a78084639f7368
deepsj1012 <deepsj1012@naver.com>
2026-01-07 21:00:05 +0900

    gitignore 추가 및 docker 컨테이너 빌드 이슈 해결

--- eureka-server/src/main/java/com/service/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/service/eurekaserver/EurekaServerApplication.java
@@ -2,9 +2,9 @@ package com.service.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaServerApplication {
 
